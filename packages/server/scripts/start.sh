0df440b1be7ebac0e1a752068a9ad8c741e52e48
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-01-10 03:45:49 +0900

    chore: CI/CD test

--- packages/server/scripts/start.sh
+++ packages/server/scripts/start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-node /home/ubuntu/build-be/dist/main.js
+CURRENT_PID=$(pgrep -f node)
+if [ -z $CURRENT_PID]
+then
+  echo "node 실행중이 아님" >> /home/ubuntu/nodelog.log
+  node /home/ubuntu/build-be/dist/main.js
+else
+  kill -15 $CURRENT_PID
+  sleep 3
+  node /home/ubuntu/build-be/dist/main.js
+fi
