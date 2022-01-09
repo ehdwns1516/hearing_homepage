d7e54fd1350c3861d524408c7f66f4f551f5b10c
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-01-10 03:22:40 +0900

    chore: CI/CD test

--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -13,7 +13,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn github action
+          Master github action
         </a>
       </header>
     </div>
