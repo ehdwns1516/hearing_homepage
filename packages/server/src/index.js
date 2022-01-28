ff39873c3efeaeed08f4944f99ba9cab2568deea
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-01-28 14:44:36 +0900

    chore: cors 테스트

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -7,12 +7,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(
-  cors({
-    origin: ['http://localhost:8080', 'http://localhost:3000', 'http://13.125.53.163:80'],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api/address', addressRouter);
