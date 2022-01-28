7512e7b0cac5ffc2e60985603a9c090a84263c75
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-01-28 15:12:03 +0900

    chore: cors 테스트

--- packages/app/src/apis/defaultAxios.js
+++ packages/app/src/apis/defaultAxios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'http://localhost:3001/api';
+axios.defaults.baseURL = 'http://13.125.53.163:3001/api';
 axios.defaults.withCredentials = true;
 
 export default axios;
